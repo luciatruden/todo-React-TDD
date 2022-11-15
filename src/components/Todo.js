72c49106fe14a8b52011d0b3ad49cb6bf7316dfc
Lucia Trujillo <110251375+luciatruden@users.noreply.github.com>
2022-11-15 17:34:52 +0100

    create Todo functional component

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Todo.css';
+
+function Todo(props){
+
+    return (
+        <div className="Todo">
+            
+        </div>
+    );
+}
+
+export default Todo;
